5c05438b215c88731c9da094fb4c70589c0c0bb6
satyam <raj@gmail.com>
2022-02-05 12:39:51 +0530

    task57 Stand in Line

--- src/index.js
+++ src/index.js
@@ -281,5 +281,19 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7);
+\function nextInLine(arr, item) {
+    // Your code here
+    arr.push(item);
+    var removed = arr.shift();
+    return removed; // Change this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
 
 
