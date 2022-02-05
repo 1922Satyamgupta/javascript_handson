bccb52ccf4c40e688bb8329db7e4eed6a337b3a7
satyam <raj@gmail.com>
2022-02-05 13:20:20 +0530

    task76 Selecting from Many Options with Switch Statements

--- src/index.js
+++ src/index.js
@@ -520,3 +520,22 @@ function testStrictNotEqual(val) {
   
   // Change these values to test
   golfScore(5, 4);  
+  function caseInSwitch(val) {
+    switch (val) {
+      case 1:
+        return "alpha";
+        break;
+      case 2:
+        return "beta";
+        break;
+      case 3:
+        return "gamma";
+        break;
+      case 4:
+        return "delta";
+        break;
+    }
+  }
+  
+  // Change this value to test
+  caseInSwitch(1); 
