ed2fb9ce51cd409e8a6c69806238607035f4e1c2
satyam <raj@gmail.com>
2022-02-05 13:37:47 +0530

    task80 Returning Boolean Values from Functions

--- src/index.js
+++ src/index.js
@@ -609,3 +609,10 @@ function testStrictNotEqual(val) {
   }
   
   chainToSwitch(7); 
+  function isLess(a, b) {
+    // Only change code below this line
+      return a <= b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);  
