723b43f550e559e057149bfc6e06e321632fdd35
satyam <raj@gmail.com>
2022-02-05 13:59:16 +0530

    task84 acces the properties

--- src/index.js
+++ src/index.js
@@ -670,4 +670,14 @@ function abTest(a, b) {
   
   
     // Only change code above this line
-  };  
+  }; 
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line   
