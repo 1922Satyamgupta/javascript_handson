15ee5264e32d2612e3fad37c09a6797224a8233f
satyam <raj@gmail.com>
2022-02-05 13:23:58 +0530

    task77 Adding a Default Option in Switch Statements

--- src/index.js
+++ src/index.js
@@ -539,3 +539,22 @@ function testStrictNotEqual(val) {
   
   // Change this value to test
   caseInSwitch(1); 
+  function switchOfStuff(val) {
+    var answer = "";
+  
+    switch (val) {
+      case "a":
+        answer = "apple";
+        break;
+      case "b":
+        answer = "bird";
+        break;
+      case "c":
+        answer = "cat";
+        break;
+      default:
+        answer = "stuff";
+    }
+  
+    return answer;
+  }  
