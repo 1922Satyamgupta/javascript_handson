ac1f64095e2e338380d0bd7496eeffa1b9de48ba
satyam <raj@gmail.com>
2022-02-05 12:27:20 +0530

    task52 Global Scope and Functions

--- src/index.js
+++ src/index.js
@@ -216,7 +216,26 @@ function timesFive(a){
   timesFive(5);
   timesFive(2);
   timesFive(0);
+// Declare the myGlobal variable below this line
+let myGlobal=10;
+let oopsGlobal;
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal=5;
+}
 
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
 
 
 
