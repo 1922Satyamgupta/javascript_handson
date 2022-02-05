cbfe15984e1330c41761900dc453a957c124f37a
satyam <raj@gmail.com>
2022-02-05 14:57:43 +0530

    task97 Iterate with JavaScript For Loops

--- src/index.js
+++ src/index.js
@@ -844,6 +844,13 @@ var i = 5;
 while(i >= 0) {
   myArray.push(i);
   i--;
+}\
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (var i = 1; i <=5; i++) {
+  myArray.push(i);
 }
       
              
